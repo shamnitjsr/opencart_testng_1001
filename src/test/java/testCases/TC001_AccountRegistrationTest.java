e85e7b2b8c30002d969bbc5193224d9445013da8
shamnitjsr <shamnitjsr@gmail.com>
2024-11-07 13:21:42 +0530

    Registration.java implemented

---
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -0,0 +1,80 @@
+package testCases;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+
+public class TC001_AccountRegistrationTest {
+
+	public WebDriver driver;
+
+	@BeforeClass
+	void setup() {
+
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
+
+	}
+
+	@AfterClass
+	void tearDown() {
+
+		driver.quit();
+	}
+
+	@Test()
+	void verify_account_registration() {
+
+		HomePage hp = new HomePage(driver);
+		hp.clickMyAccount();
+		hp.clickRegister();
+
+		AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
+		regpage.setFirstName(randomeString().toUpperCase());
+		regpage.setLastName(randomeString().toUpperCase());
+		regpage.setEmail(randomeString() +"@gamil.com");
+		regpage.setTelephone(randomeNumber());
+
+		String password = randomealphanumeric();
+
+		regpage.setPassword(password);
+		regpage.setConfirmPassword(password);
+
+		regpage.setPrivacyPolicy();
+		regpage.clickContinue();
+
+		String confmsg = regpage.getConfirmationMsg();
+
+		Assert.assertEquals(confmsg, "Your Account Has Been Created!");
+	}
+
+	public static String randomeString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		return generatedString;
+	}
+
+	public static String randomeNumber() {
+		String generatedNumber = RandomStringUtils.randomNumeric(10);
+		return generatedNumber;
+	}
+
+	public static String randomealphanumeric() {
+		String generateString = RandomStringUtils.randomAlphabetic(5);
+		String generatedNumber = RandomStringUtils.randomNumeric(5);
+		return (generateString + generatedNumber);
+	}
+
+}
