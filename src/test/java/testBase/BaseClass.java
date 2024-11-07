8be4a1aad331144458f0091a2db977905ff13895
shamnitjsr <shamnitjsr@gmail.com>
2024-11-07 21:42:19 +0530

    Testng implementation completed

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -12,17 +13,20 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 public class BaseClass {
 
-	public WebDriver driver;
+	public static WebDriver driver;
 
 	public Logger logger; // Log4j
 
@@ -33,40 +37,53 @@ public class BaseClass {
 
 	public void setup(String os, String br) throws IOException {
 
-		// loading config.properties file
-
+		// Loading config.propeerties file
 		FileReader file = new FileReader("./src//test//resources//config.properties");
 		p = new Properties();
 		p.load(file);
 
 		logger = LogManager.getLogger(this.getClass());
 
-		switch (br.toLowerCase()) {
+		if (p.getProperty("execution_env").equalsIgnoreCase("remote")) {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
 
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "firefox":
-			driver = new FirefoxDriver();
-			break;
-		case "edge":
-			driver = new ChromeDriver();
-			break;
-		default:
-			System.out.println("Invalid Browser name..");
-			return;
+			// os
+			if (os.equalsIgnoreCase("windows")) {
+
+				capabilities.setPlatform(Platform.WIN11);
+
+			} else if (os.equalsIgnoreCase("mac")) {
+				capabilities.setPlatform(Platform.MAC);
+			} else if (os.equalsIgnoreCase("linux")) {
+				capabilities.setPlatform(Platform.LINUX);
+			} else {
+
+				System.out.println("No Mathcing os");
+			}
+
+			// browser
+
+			switch (br.toLowerCase()) {
+			case "chrome":
+				capabilities.setBrowserName("chrome");
+				break;
+			case "edge":
+				capabilities.setBrowserName("MicrosoftEdge");
+				break;
+			case "firefox":
+				capabilities.setBrowserName("firefox");
+				break;
+			default:
+				System.out.println("No Matching browser");
+				return;
+			}
+
+			driver = new RemoteWebDriver(new URL("http://192.168.1.5:4444/wd/hub"), capabilities);
 
 		}
-
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-		driver.get(p.getProperty("appURL"));
-		driver.manage().window().maximize();
-
 	}
 
-	@AfterClass(groups = { "Sanity", "Regression", "Master"})
+	@AfterClass(groups = { "Sanity", "Regression", "Master" })
 	public void tearDown() {
 
 		driver.quit();
